c60ecfced90a079671a82c37198cac87664306ee
precious <kshitijjbbhatia@gmail.com>
2022-04-02 20:04:59 +0530

    ignore chakra area

--- create_flag.py
+++ create_flag.py
@@ -11,6 +11,8 @@ def get_rectangle_coordinates(* ,top_y, bottom_y, left_x, right_x, colour):
 
 saffron = get_rectangle_coordinates(top_y = 299,bottom_y= 315,left_x= 238,right_x= 541,colour= 3)
 white = get_rectangle_coordinates(top_y= 316,bottom_y= 329,left_x= 238,right_x= 541,colour= 31)
+# remove pixels in chakra area for now
+white = list(filter(lambda v: v[0]>392 or v[0]<376, white))
 green = get_rectangle_coordinates(top_y= 330,bottom_y= 342,left_x= 238,right_x= 541,colour= 6)
 
 flag = saffron + white + green
