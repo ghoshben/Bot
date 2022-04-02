3c98bc415c20bb3e338d6b425820f47bd7a1868a
Dhravya <dhravyashah@gmail.com>
2022-04-02 17:45:39 +0530

    fixed formatting

--- create_flag.py
+++ create_flag.py
@@ -1,13 +1,13 @@
-import json 
+import json
+
 
 def get_rectangle_coordinates(top_y, bottom_y, left_x, right_x, colour):
+    """Gets the coordinates of the rectangle to create a flag."""
     coords = []
     for y in range(top_y, bottom_y+1):
         for x in range(left_x, right_x+1):
             coords.append([x, y, colour])
     return coords
-
-
 
 saffron = get_rectangle_coordinates(299, 315, 238, 541, 3)
 white = get_rectangle_coordinates(316, 239, 238, 541, 31)
@@ -17,9 +17,3 @@ flag = saffron + white + green
 
 with open("orders.json", "w") as f:
     json.dump(flag, f, indent=4)
-
-
-
-
-
-
--- placeindiabot.user.js
+++ placeindiabot.user.js
@@ -19,41 +19,41 @@
 
 var placeOrders = [];
 var accessToken;
-var canvas = document.createElement('canvas');
+var canvas = document.createElement("canvas");
 
 const COLOR_MAPPINGS = {
-	'#FF4500': 2,
-	'#FFA800': 3,
-	'#FFD635': 4,
-	'#00A368': 6,
-	'#7EED56': 8,
-	'#2450A4': 12,
-	'#3690EA': 13,
-	'#51E9F4': 14,
-	'#811E9F': 18,
-	'#B44AC0': 19,
-	'#FF99AA': 23,
-	'#9C6926': 25,
-	'#000000': 27,
-	'#898D90': 29,
-	'#D4D7D9': 30,
-	'#FFFFFF': 31
+	"#FF4500": 2,
+	"#FFA800": 3,
+	"#FFD635": 4,
+	"#00A368": 6,
+	"#7EED56": 8,
+	"#2450A4": 12,
+	"#3690EA": 13,
+	"#51E9F4": 14,
+	"#811E9F": 18,
+	"#B44AC0": 19,
+	"#FF99AA": 23,
+	"#9C6926": 25,
+	"#000000": 27,
+	"#898D90": 29,
+	"#D4D7D9": 30,
+	"#FFFFFF": 31,
 };
 
 (async function () {
-	GM_addStyle(GM_getResourceText('TOASTIFY_CSS'));
+	GM_addStyle(GM_getResourceText("TOASTIFY_CSS"));
 	canvas.width = 1000;
 	canvas.height = 1000;
 	canvas = document.body.appendChild(canvas);
 
 	Toastify({
-		text: 'Getting access Token...',
-		duration: 10000
+		text: "Getting access Token...",
+		duration: 10000,
 	}).showToast();
 	accessToken = await getAccessToken();
 	Toastify({
-		text: 'Access token fetched!',
-		duration: 10000
+		text: "Access token fetched!",
+		duration: 10000,
 	}).showToast();
 
 	setInterval(updateOrders, 5 * 60 * 1000); // Update orders elke vijf minuten.
@@ -67,10 +67,10 @@ async function attemptPlace() {
 		const canvasUrl = await getCurrentImageUrl();
 		ctx = await getCanvasFromUrl(canvasUrl);
 	} catch (e) {
-		console.warn('Fout bij ophalen map: ', e);
+		console.warn("Unexpected error: ", e);
 		Toastify({
-			text: 'Error retrieving folder. Try again in 15 sec.',
-			duration: 10000
+			text: "Error retrieving folder. Try again in 15 sec.",
+			duration: 10000,
 		}).showToast();
 		setTimeout(attemptPlace, 15000); // probeer opnieuw in 15sec.
 		return;
@@ -81,117 +81,137 @@ async function attemptPlace() {
 		const y = order[1];
 		const colorId = order[2];
 		const rgbaAtLocation = ctx.getImageData(x, y, 1, 1).data;
-		const hex = rgbToHex(rgbaAtLocation[0], rgbaAtLocation[1], rgbaAtLocation[2]);
+		const hex = rgbToHex(
+			rgbaAtLocation[0],
+			rgbaAtLocation[1],
+			rgbaAtLocation[2]
+		);
 		const currentColorId = COLOR_MAPPINGS[hex];
 		// Deze pixel klopt al.
 		if (currentColorId == colorId) continue;
 
 		Toastify({
 			text: `Trying to post pixel to ${x}, ${y}...`,
-			duration: 10000
+			duration: 10000,
 		}).showToast();
 		await place(x, y, colorId);
 
 		Toastify({
 			text: `Waiting for cooldown...`,
-			duration: 315000
+			duration: 315000,
 		}).showToast();
 		setTimeout(attemptPlace, 315000); // 5min en 15sec, just to be safe.
 		return;
 	}
 
 	Toastify({
-		text: 'All pixels are already in the right place!',
-		duration: 10000
+		text: "All pixels are already in the right place!",
+		duration: 10000,
 	}).showToast();
 	setTimeout(attemptPlace, 30000); // probeer opnieuw in 30sec.
 }
 
 function updateOrders() {
-	fetch('https://raw.githubusercontent.com/PreciousWarrior/Bot/master/orders.json').then(async (response) => {
-		if (!response.ok) return console.warn('Unable to fetch orders! (non-ok status code)');
-		const data = await response.json();
+	fetch(
+		"https://raw.githubusercontent.com/PreciousWarrior/Bot/master/orders.json"
+	)
+		.then(async (response) => {
+			if (!response.ok)
+				return console.warn("Unable to fetch orders! (non-ok status code)");
+			const data = await response.json();
 
-		if (JSON.stringify(data) !== JSON.stringify(placeOrders)) {
-			Toastify({
-				text: `New orders loaded. Total pixels: ${data.length}.`,
-				duration: 10000
-			}).showToast();
-		}
+			if (JSON.stringify(data) !== JSON.stringify(placeOrders)) {
+				Toastify({
+					text: `New orders loaded. Total pixels: ${data.length}.`,
+					duration: 10000,
+				}).showToast();
+			}
 
-		placeOrders = data;
-	}).catch((e) => console.warn('Unable to fetch orders!', e));
+			placeOrders = data;
+		})
+		.catch((e) => console.warn("Unable to fetch orders!", e));
 }
 
 function place(x, y, color) {
-	return fetch('https://gql-realtime-2.reddit.com/query', {
-		method: 'POST',
+	return fetch("https://gql-realtime-2.reddit.com/query", {
+		method: "POST",
 		body: JSON.stringify({
-			'operationName': 'setPixel',
-			'variables': {
-				'input': {
-					'actionName': 'r/replace:set_pixel',
-					'PixelMessageData': {
-						'coordinate': {
-							'x': x,
-							'y': y
+			operationName: "setPixel",
+			variables: {
+				input: {
+					actionName: "r/replace:set_pixel",
+					PixelMessageData: {
+						coordinate: {
+							x: x,
+							y: y,
 						},
-						'colorIndex': color,
-						'canvasIndex': 0
-					}
-				}
+						colorIndex: color,
+						canvasIndex: 0,
+					},
+				},
 			},
-			'query': 'mutation setPixel($input: ActInput!) {\n  act(input: $input) {\n    data {\n      ... on BasicMessage {\n        id\n        data {\n          ... on GetUserCooldownResponseMessageData {\n            nextAvailablePixelTimestamp\n            __typename\n          }\n          ... on SetPixelResponseMessageData {\n            timestamp\n            __typename\n          }\n          __typename\n        }\n        __typename\n      }\n      __typename\n    }\n    __typename\n  }\n}\n'
+			query:
+				"mutation setPixel($input: ActInput!) {\n  act(input: $input) {\n    data {\n      ... on BasicMessage {\n        id\n        data {\n          ... on GetUserCooldownResponseMessageData {\n            nextAvailablePixelTimestamp\n            __typename\n          }\n          ... on SetPixelResponseMessageData {\n            timestamp\n            __typename\n          }\n          __typename\n        }\n        __typename\n      }\n      __typename\n    }\n    __typename\n  }\n}\n",
 		}),
 		headers: {
-			'origin': 'https://hot-potato.reddit.com',
-			'referer': 'https://hot-potato.reddit.com/',
-			'apollographql-client-name': 'mona-lisa',
-			'Authorization': `Bearer ${accessToken}`,
-			'Content-Type': 'application/json'
-		}
+			origin: "https://hot-potato.reddit.com",
+			referer: "https://hot-potato.reddit.com/",
+			"apollographql-client-name": "mona-lisa",
+			Authorization: `Bearer ${accessToken}`,
+			"Content-Type": "application/json",
+		},
 	});
 }
 
 async function getAccessToken() {
-	const usingOldReddit = window.location.href.includes('new.reddit.com');
-	const url = usingOldReddit ? 'https://new.reddit.com/r/place/' : 'https://www.reddit.com/r/place/';
+	const usingOldReddit = window.location.href.includes("new.reddit.com");
+	const url = usingOldReddit
+		? "https://new.reddit.com/r/place/"
+		: "https://www.reddit.com/r/place/";
 	const response = await fetch(url);
 	const responseText = await response.text();
 
 	// TODO: ew
-	return responseText.split('\"accessToken\":\"')[1].split('"')[0];
+	return responseText.split('"accessToken":"')[1].split('"')[0];
 }
 
 async function getCurrentImageUrl() {
 	return new Promise((resolve, reject) => {
-		const ws = new WebSocket('wss://gql-realtime-2.reddit.com/query', 'graphql-ws');
+		const ws = new WebSocket(
+			"wss://gql-realtime-2.reddit.com/query",
+			"graphql-ws"
+		);
 
 		ws.onopen = () => {
-			ws.send(JSON.stringify({
-				'type': 'connection_init',
-				'payload': {
-					'Authorization': `Bearer ${accessToken}`
-				}
-			}));
-			ws.send(JSON.stringify({
-				'id': '1',
-				'type': 'start',
-				'payload': {
-					'variables': {
-						'input': {
-							'channel': {
-								'teamOwner': 'AFD2022',
-								'category': 'CANVAS',
-								'tag': '0'
-							}
-						}
+			ws.send(
+				JSON.stringify({
+					type: "connection_init",
+					payload: {
+						Authorization: `Bearer ${accessToken}`,
 					},
-					'extensions': {},
-					'operationName': 'replace',
-					'query': 'subscription replace($input: SubscribeInput!) {\n  subscribe(input: $input) {\n    id\n    ... on BasicMessage {\n      data {\n        __typename\n        ... on FullFrameMessageData {\n          __typename\n          name\n          timestamp\n        }\n      }\n      __typename\n    }\n    __typename\n  }\n}'
-				}
-			}));
+				})
+			);
+			ws.send(
+				JSON.stringify({
+					id: "1",
+					type: "start",
+					payload: {
+						variables: {
+							input: {
+								channel: {
+									teamOwner: "AFD2022",
+									category: "CANVAS",
+									tag: "0",
+								},
+							},
+						},
+						extensions: {},
+						operationName: "replace",
+						query:
+							"subscription replace($input: SubscribeInput!) {\n  subscribe(input: $input) {\n    id\n    ... on BasicMessage {\n      data {\n        __typename\n        ... on FullFrameMessageData {\n          __typename\n          name\n          timestamp\n        }\n      }\n      __typename\n    }\n    __typename\n  }\n}",
+					},
+				})
+			);
 		};
 
 		ws.onmessage = (message) => {
@@ -199,12 +219,17 @@ async function getCurrentImageUrl() {
 			const parsed = JSON.parse(data);
 
 			// TODO: ew
-			if (!parsed.payload || !parsed.payload.data || !parsed.payload.data.subscribe || !parsed.payload.data.subscribe.data) return;
+			if (
+				!parsed.payload ||
+				!parsed.payload.data ||
+				!parsed.payload.data.subscribe ||
+				!parsed.payload.data.subscribe.data
+			)
+				return;
 
 			ws.close();
 			resolve(parsed.payload.data.subscribe.data.name);
-		}
-
+		};
 
 		ws.onerror = reject;
 	});
@@ -212,9 +237,9 @@ async function getCurrentImageUrl() {
 
 function getCanvasFromUrl(url) {
 	return new Promise((resolve, reject) => {
-		var ctx = canvas.getContext('2d');
+		var ctx = canvas.getContext("2d");
 		var img = new Image();
-		img.crossOrigin = 'anonymous';
+		img.crossOrigin = "anonymous";
 		img.onload = () => {
 			ctx.drawImage(img, 0, 0);
 			resolve(ctx);
@@ -225,5 +250,8 @@ function getCanvasFromUrl(url) {
 }
 
 function rgbToHex(r, g, b) {
-	return '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1).toUpperCase();
+	return (
+		"#" +
+		((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1).toUpperCase()
+	);
 }
